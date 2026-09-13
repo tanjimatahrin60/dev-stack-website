7cba3b138b26133e327b0f0e27888230aa100a1a
tanjimatahrin60 <tahrintanjima60@gmail.com>
2026-09-13 17:16:16 +0600

    added tech stack types

---
+++ src/types/TechStackTypes.ts
@@ -0,0 +1,10 @@
+export default interface TechDataTypes {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  level: string;
+  rating: number;
+  badge: string;
+  icon: string;
+}
